130baf98a7d8166eac692aac025fd9c867420fe4
uSasha <sasha.engineer@gmail.com>
2018-01-14 22:18:56 +0300

    initial project srtructure

---
+++ recommedation_service/recommendation_app.py
@@ -0,0 +1,36 @@
+import pika
+import random
+
+
+def predict(user_id):
+    random.random.seed(user_id)
+    random.choice(['Led Zeppelin',
+                             'Jefferson Airplane',
+                             'Sixto Rodriguez',
+                             'Jimi Hendrix',
+                             'Chad VanGaalen'])
+
+
+def on_request(ch, method, props, body):
+    user_id = int(body)
+
+    print("recommendation for user", user_id)
+    response = predict(user_id)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id
+                                                     =props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='rpc_queue')
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+print("recommendation service ready")
+channel.start_consuming()
---
+++ web_server/recommedation_client.py
@@ -0,0 +1,43 @@
+import pika
+import uuid
+import numpy as np
+
+
+class RpcClient(object):
+    def __init__(self):
+        self.response = None
+        self.corr_id = None
+
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to=self.callback_queue,
+                                         correlation_id=self.corr_id,
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+
+recommender = RpcClient()
+
+user_id = np.random.randint(0, 1000)
+print("requesting recommendation for user", user_id)
+response = recommender.call(user_id)
+print("turning on", response)
+recommender.connection.close()
