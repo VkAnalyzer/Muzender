8b98c756bbe029e14b319b69dd4cffcf76612803
root <xsile2009@gmail.com>
2018-01-16 14:32:00 +0300

    page render

--- web_server/rest_api/rest_api/urls.py
+++ web_server/rest_api/rest_api/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^music/', views.music_recommendation, name='music')
 ]
---
+++ web_server/rest_api/rest_api/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+
+def music_recommendation(user_id):
+    return HttpResponse('Музыка')
