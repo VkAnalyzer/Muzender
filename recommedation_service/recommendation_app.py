4a725381fbb4cfa9d17662eae0b9c34483800c2f
uSasha <sasha.engineer@gmail.com>
2018-02-05 22:39:08 +0300

    rpc client moved to separate file

--- recommedation_service/recommendation_app.py
+++ recommedation_service/recommendation_app.py
@@ -5,6 +5,7 @@ from scipy import sparse
 import implicit
 import pandas as pd
 import numpy as np
+from rpc_client import RpcClient
 
 
 def predict(user_id):
@@ -53,38 +54,6 @@ def on_request(ch, method, props, body):
                                                      =props.correlation_id),
                      body=str(response))
     ch.basic_ack(delivery_tag=method.delivery_tag)
-
-
-# TODO move to separate file, add queue parameters
-class RpcClient(object):
-    def __init__(self):
-        self.response = None
-        self.corr_id = None
-
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='queue'))
-        self.channel = self.connection.channel()
-        result = self.channel.queue_declare(exclusive=True)
-        self.callback_queue = result.method.queue
-        self.channel.basic_consume(self.on_response, no_ack=True,
-                                   queue=self.callback_queue)
-
-    def on_response(self, ch, method, props, body):
-        if self.corr_id == props.correlation_id:
-            self.response = body
-
-    def call(self, n):
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        self.channel.basic_publish(exchange='',
-                                   routing_key='rpc_user_music',
-                                   properties=pika.BasicProperties(
-                                         reply_to=self.callback_queue,
-                                         correlation_id=self.corr_id,
-                                         ),
-                                   body=str(n))
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
 
 
 if __name__ == '__main__':
