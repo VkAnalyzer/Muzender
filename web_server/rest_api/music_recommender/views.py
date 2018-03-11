d4b53f46950d3b277f230e0234210e2ae1e2d285
uSasha <sasha.engineer@gmail.com>
2018-03-11 20:01:49 +0300

    web page send parameter dictionary to recommender app

--- web_server/rest_api/music_recommender/views.py
+++ web_server/rest_api/music_recommender/views.py
@@ -5,7 +5,7 @@ def index_page(request, user_id='', predicted_song=''):
     recommender = rc.RpcClient()
     if request.method == "POST":
         user_id = request.POST["text"]
-        predicted_song = recommender.call(user_id)
+        predicted_song = recommender.call({'user_id': user_id})
 
     return render(request, 'music_recommender/music_template.html', {'predicted_song': predicted_song,
                                                                      'user_id': user_id})
