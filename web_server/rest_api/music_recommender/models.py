30054b2f2af827896f930b22defd52f74a4d8bd4
root <xsile2009@gmail.com>
2018-04-05 22:16:22 +0300

    models and serializers added

--- web_server/rest_api/music_recommender/models.py
+++ web_server/rest_api/music_recommender/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class VkProfile(models.Model):
+  modified = models.DateTimeField(auto_now=True)
+  vk_id = models.CharField(max_length=30)
+
+
+class Band(models.Model)
+  user = models.ForeignKey(VkProfile, related_name='bands')
+  band_name = models.CharField(max_length=128)
+
+
+class Song(models.Model):
+  band = models.ForeignKey(Bands, related_name='songs')
+  song_name = models.CharField(max_length=128)
