e4b507a585cf7ecfb3e4ac6af67f942538627b22
root <xsile2009@gmail.com>
2018-04-06 00:20:06 +0300

    override some views methods

--- web_server/rest_api/music_recommender/views.py
+++ web_server/rest_api/music_recommender/views.py
@@ -1,5 +1,8 @@
 from recommedation_client.rpc_client import RpcClient
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import VkProfile, Band, Song
+from .serializers import VkProfileSerializer, BandSerializer, SongSerializer
 
 
 def index_page(request, user_id='', predicted_song=''):
@@ -11,3 +14,14 @@ def index_page(request, user_id='', predicted_song=''):
 
     return render(request, 'music_recommender/music_template.html', {'predicted_song': predicted_song,
                                                                      'user_id': user_id})
+
+
+class VkProfileViewSet(viewsets.ModelViewSet):
+  queryset = VkProfile.objects.all()
+  serializer_class = VkProfileSerializer
+
+  def perform_create(self, serializer):
+    user_id = self.request.data['vk_id']
+    if VkProfile.objects.filter(vk_id=user_id).exists():
+      self.destroy(self.request)
+    serializer.save()
