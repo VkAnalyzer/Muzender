c62460d06f6d594731ff23b8caae278cfc6d5e4d
root <xsile2009@gmail.com>
2018-10-06 18:08:22 +0300

    new web_server

---
+++ web_server/web_server/music_recommender/views.py
@@ -0,0 +1,19 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../../")
+
+from recommedation_service.rpc_client import RpcClient
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+
+
+@api_view(['GET', 'POST'])
+def get_recommendation(request):
+
+  if request.method == 'POST':
+    #recommender = RpcClient(host='queue', routing_key='rpc_recommendations')
+    #request_data = JSONParser().parse(request)
+    #user_id = request_data["user_id"]
+    #predicted_bands = recommender.call({'user_id': user_id})
+    predicted_bands = ['545', '3434', 'asdasd']
+    return JsonResponse(predicted_bands, safe=False)
