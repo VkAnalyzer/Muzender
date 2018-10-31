c695f76d9b91e35eaa0a1c6074f062283643f317
root <xsile2009@gmail.com>
2018-10-31 14:58:01 +0300

    rpc client modified

--- web_server/web_server/music_recommender/views.py
+++ web_server/web_server/music_recommender/views.py
@@ -1,16 +1,20 @@
-from recommendation_service.rpc_client import RpcClient
+from recommendation_service.rpc_client import connection, channel, RpcClient
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+import time
+import random
 
 
 @api_view(['GET', 'POST'])
 def get_recommendation(request):
 
   if request.method == 'POST':
-    recommender = RpcClient(host='queue', routing_key='rpc_recommendations')
     request_data = JSONParser().parse(request)
-    user_id = request_data["user_id"]
-    predicted_bands = recommender.call({'user_id': user_id})
-    #predicted_bands = ['545', '3434', 'asdasd']
+    user_id = request_data['user_id']
+
+    rpc_client = RpcClient(connection=connection, channel=channel, routing_key='user_id')
+    predicted_bands = rpc_client.call({'user_id': user_id})
+    #time.sleep(5)
+    #predicted_bands = ['545', '3434', str(random.randint(1, 100))]
     return JsonResponse(predicted_bands, safe=False)
--- web_server/web_server/recommendation_service/rpc_client.py
+++ web_server/web_server/recommendation_service/rpc_client.py
@@ -2,15 +2,18 @@ import uuid
 import pika
 import pickle
 
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='queue'))
+channel = connection.channel()
 
-class RpcClient(object):
-    def __init__(self, host, routing_key):
+class RpcClient:
+
+    def __init__(self, connection, channel, routing_key):
+        self.connection = connection
+        self.channel = channel
         self.routing_key = routing_key
         self.response = None
         self.corr_id = None
 
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
-        self.channel = self.connection.channel()
         result = self.channel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
         self.channel.basic_consume(self.on_response, no_ack=True,
@@ -20,7 +23,7 @@ class RpcClient(object):
         if self.corr_id == props.correlation_id:
             self.response = pickle.loads(body)
 
-    def call(self, n):
+    def call(self, body):
         self.response = None
         self.corr_id = str(uuid.uuid4())
         self.channel.basic_publish(exchange='',
@@ -29,7 +32,8 @@ class RpcClient(object):
                                          reply_to=self.callback_queue,
                                          correlation_id=self.corr_id,
                                          ),
-                                   body=pickle.dumps(n))
+                                   body=pickle.dumps(body))
         while self.response is None:
             self.connection.process_data_events()
+        self.channel.queue_delete(queue=self.callback_queue)
         return self.response
