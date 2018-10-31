ca85fbacd53ee7c6e3b7fe9247bcba18288359a5
root <xsile2009@gmail.com>
2018-10-31 20:15:04 +0300

    rpc changed

--- web_server/web_server/music_recommender/views.py
+++ web_server/web_server/music_recommender/views.py
@@ -1,4 +1,4 @@
-from recommendation_service.rpc_client import connection, channel, RpcClient
+from recommendation_service.rpc_client import RpcClient
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
@@ -8,13 +8,12 @@ import random
 
 @api_view(['GET', 'POST'])
 def get_recommendation(request):
+    if request.method == 'POST':
+        request_data = JSONParser().parse(request)
+        user_id = request_data['user_id']
 
-  if request.method == 'POST':
-    request_data = JSONParser().parse(request)
-    user_id = request_data['user_id']
-
-    rpc_client = RpcClient(connection=connection, channel=channel, routing_key='user_id')
-    predicted_bands = rpc_client.call({'user_id': user_id})
-    #time.sleep(5)
-    #predicted_bands = ['545', '3434', str(random.randint(1, 100))]
-    return JsonResponse(predicted_bands, safe=False)
+        rpc_client = RpcClient(routing_key='user_id', host='queue')
+        predicted_bands = rpc_client.call({'user_id': user_id})
+        # time.sleep(5)
+        # predicted_bands = ['545', '3434', str(random.randint(1, 100))]
+        return JsonResponse(predicted_bands, safe=False)
--- web_server/web_server/recommendation_service/rpc_client.py
+++ web_server/web_server/recommendation_service/rpc_client.py
@@ -2,21 +2,19 @@ import uuid
 import pika
 import pickle
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='queue'))
-channel = connection.channel()
 
 class RpcClient:
 
-    def __init__(self, connection, channel, routing_key):
-        self.connection = connection
-        self.channel = channel
+    def __init__(self, routing_key, host='queue'):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        self.channel = self.connection.channel()
         self.routing_key = routing_key
         self.response = None
         self.corr_id = None
 
         result = self.channel.queue_declare(exclusive=True)
         self.callback_queue = result.method.queue
-        self.consumer_tag = self.channel.basic_consume(self.on_response, no_ack=True,
+        self.consumer_tag = self.channel.basic_consume(self.on_response, no_ack=True, auto_delete=True,
                                                        queue=self.callback_queue)
 
     def on_response(self, ch, method, props, body):
@@ -29,12 +27,13 @@ class RpcClient:
         self.channel.basic_publish(exchange='',
                                    routing_key=self.routing_key,
                                    properties=pika.BasicProperties(
-                                         reply_to=self.callback_queue,
-                                         correlation_id=self.corr_id,
-                                         ),
+                                       reply_to=self.callback_queue,
+                                       correlation_id=self.corr_id,
+                                   ),
                                    body=pickle.dumps(body))
         while self.response is None:
             self.connection.process_data_events()
         self.channel.basic_cancel(self.consumer_tag)
         self.channel.queue_delete(queue=self.callback_queue)
+        self.connection.close()
         return self.response
